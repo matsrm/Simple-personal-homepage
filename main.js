64320b4b1b739a9e2a27e52ece5571a78f81a63b
matsrm <73002270+matsrm@users.noreply.github.com>
2023-11-29 22:04:33 +0100

    Generating dot navigation based on number of slides

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ const slides = document.getElementsByClassName(".slide");
 const dotsContainer = document.querySelector(".dot-navigation");
 let currentSlide = 0;
 let slideInterval;
+
+slides[currentSlide].classList.add("active");
+
+slides.forEach((slide, index) => {
+    const dot = document.createElement("span");
+    dot.classList.add("dot-navigation-item");
+    dot.addEventListener("click", () => navigateToSlide(index));
+    dotsContainer.appendChild(dot);
+
+});
+
